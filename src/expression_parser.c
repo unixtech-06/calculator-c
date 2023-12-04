874ded25eda87f8e6fc5d8012ece5f3e5e8f1651
Unixtech-06 <ryosuke_666@icloud.com>
2023-12-05 01:30:56 +0900

    add license

--- src/expression_parser.c
+++ src/expression_parser.c
@@ -1,95 +1,133 @@
+/*	$Project: calculator-c, Version: 2023, Last Maintenance: 2023/12/5 1:25 $	*/
+/*
+ * Copyright (c) 2023, Ryosuke
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author's Email: test@gmail.com
+ * File: expression_parser.c
+ */
+
+
 #include "../lib/expression_parser.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 
-static 
-int get_precedence(char op) 
+/* Get the precedence of an operator. Higher values for higher precedence. */
+static int
+get_precedence(char op)
 {
-    switch (op) {
-    case '*':
-    case '/':
-        return 2;
-    case '+':
-    case '-':
-        return 1;
-    default:
-        return 0;
-    }
+	switch (op) {
+	case '*':
+	case '/':
+		return 2;
+	case '+':
+	case '-':
+		return 1;
+	default:
+		return 0;
+	}
 }
 
-static 
-double apply_operator(double val1, double val2, char op) 
+/* Apply an operator to two values and return the result. */
+static double
+apply_operator(double val1, double val2, char op)
 {
-    switch (op) {
-    case '+': return val1 + val2;
-    case '-': return val1 - val2;
-    case '*': return val1 * val2;
-    case '/': 
-        if (val2 == 0) {
-            fprintf(stderr, "Error: Division by zero.\n");
-            return 0.0;
-        }
-        return val1 / val2;
-    default: 
-        return 0.0;
-    }
+	switch (op) {
+	case '+': return val1 + val2;
+	case '-': return val1 - val2;
+	case '*': return val1 * val2;
+	case '/': 
+		if (val2 == 0) {
+			fprintf(stderr, "Error: Division by zero.\n");
+			return 0.0;
+		}
+		return val1 / val2;
+	default: 
+		return 0.0;
+	}
 }
 
-double 
-calculate_with_parentheses(const char *expression) 
+/* Calculate the value of an arithmetic expression with parentheses. */
+double
+calculate_with_parentheses(const char *expression)
 {
-    double numbers[100];
-    char operators[100];
-    int num_top = -1, op_top = -1;
+	double numbers[100]; // Stack for numbers
+	char operators[100]; // Stack for operators
+	int num_top = -1, op_top = -1; // Stack pointers
 
-    for (int i = 0; expression[i] != '\0'; i++) {
-        if (isspace(expression[i]))
-            continue;
+	for (int i = 0; expression[i] != '\0'; i++) {
+		if (isspace(expression[i]))
+			continue;
 
-        if (isdigit(expression[i])) {
-            double value = 0;
-            while (isdigit(expression[i])) {
-                value = value * 10 + (expression[i] - '0');
-                i++;
-            }
-            i--;
-            numbers[++num_top] = value;
-        } else if (expression[i] == '(') {
-            operators[++op_top] = expression[i];
-        } else if (expression[i] == ')') {
-            while (op_top != -1 && operators[op_top] != '(') {
-                double val2 = numbers[num_top--];
-                double val1 = numbers[num_top--];
-                char op = operators[op_top--];
+		if (isdigit(expression[i])) {
+			double value = 0;
+			while (isdigit(expression[i])) {
+				value = value * 10 + (expression[i] - '0');
+				i++;
+			}
+			i--;
+			numbers[++num_top] = value;
+		} else if (expression[i] == '(') {
+			operators[++op_top] = expression[i];
+		} else if (expression[i] == ')') {
+			while (op_top != -1 && operators[op_top] != '(') {
+				double val2 = numbers[num_top--];
+				double val1 = numbers[num_top--];
+				char op = operators[op_top--];
 
-                numbers[++num_top] = apply_operator(val1, val2, op);
-            }
-            if (op_top != -1) {
-                op_top--; // スタックから '(' をポップ
-            }
-        } else {
-            while (op_top != -1 && 
-                   get_precedence(operators[op_top]) >= get_precedence(expression[i])) {
-                double val2 = numbers[num_top--];
-                double val1 = numbers[num_top--];
-                char op = operators[op_top--];
+				numbers[++num_top] = apply_operator(val1, val2, op);
+			}
+			if (op_top != -1) {
+				op_top--; // Pop the '(' from the stack
+			}
+		} else {
+			while (op_top != -1 && 
+				   get_precedence(operators[op_top]) >= get_precedence(expression[i])) {
+				double val2 = numbers[num_top--];
+				double val1 = numbers[num_top--];
+				char op = operators[op_top--];
 
-                numbers[++num_top] = apply_operator(val1, val2, op);
-            }
-            operators[++op_top] = expression[i];
-        }
-    }
+				numbers[++num_top] = apply_operator(val1, val2, op);
+			}
+			operators[++op_top] = expression[i];
+		}
+	}
 
-    while (op_top != -1) {
-        double val2 = numbers[num_top--];
-        double val1 = numbers[num_top--];
-        char op = operators[op_top--];
+	while (op_top != -1) {
+		double val2 = numbers[num_top--];
+		double val1 = numbers[num_top--];
+		char op = operators[op_top--];
 
-        numbers[++num_top] = apply_operator(val1, val2, op);
-    }
+		numbers[++num_top] = apply_operator(val1, val2, op);
+	}
 
-    return numbers[num_top];
+	return numbers[num_top];
 }
+
 
--- src/main.c
+++ src/main.c
@@ -1,85 +1,120 @@
+/*	$Project: calculator-c, Version: 2023, Last Maintenance: 2023/12/5 1:25 $	*/
+/*
+ * Copyright (c) 2023, Ryosuke
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author's Email: test@gmail.com
+ * File: main.c
+ */
+
+
 #include <gtk/gtk.h>
 #include "../lib/expression_parser.h"
 
 static GtkWidget *entry;
 static GtkWidget *expression_label;
 
-static 
-void on_button_clicked(GtkWidget *widget, gpointer data) 
+/* Callback function for button click events. */
+static void
+on_button_clicked(GtkWidget *widget, gpointer data)
 {
-    const gchar *label = gtk_button_get_label(GTK_BUTTON(widget));
-    if (label == NULL) {
-        fprintf(stderr, "Error: Null label in button.\n");
-        return;
-    }
+	const gchar *label = gtk_button_get_label(GTK_BUTTON(widget));
+	if (label == NULL) {
+		fprintf(stderr, "Error: Null label in button.\n");
+		return;
+	}
 
-    const gchar *current_text = gtk_entry_get_text(GTK_ENTRY(entry));
-    if (current_text == NULL) {
-        fprintf(stderr, "Error: Null text in entry widget.\n");
-        return;
-    }
+	const gchar *current_text = gtk_entry_get_text(GTK_ENTRY(entry));
+	if (current_text == NULL) {
+		fprintf(stderr, "Error: Null text in entry widget.\n");
+		return;
+	}
 
-    if (strcmp(label, "=") == 0) {
-        gtk_label_set_text(GTK_LABEL(expression_label), current_text);
+	if (strcmp(label, "=") == 0) {
+		gtk_label_set_text(GTK_LABEL(expression_label), current_text);
 
-        double result = calculate_with_parentheses(current_text);
-        char result_str[64];
-        snprintf(result_str, sizeof(result_str), "%f", result);
-        gtk_entry_set_text(GTK_ENTRY(entry), result_str);
-    } else {
-        gchar *new_text = g_strdup_printf("%s%s", current_text, label);
-        if (new_text == NULL) {
-            fprintf(stderr, "Error: Failed to allocate memory for new text.\n");
-            return;
-        }
-        gtk_entry_set_text(GTK_ENTRY(entry), new_text);
-        g_free(new_text);
-    }
+		double result = calculate_with_parentheses(current_text);
+		char result_str[64];
+		snprintf(result_str, sizeof(result_str), "%f", result);
+		gtk_entry_set_text(GTK_ENTRY(entry), result_str);
+	} else {
+		gchar *new_text = g_strdup_printf("%s%s", current_text, label);
+		if (new_text == NULL) {
+			fprintf(stderr, "Error: Failed to allocate memory for new text.\n");
+			return;
+		}
+		gtk_entry_set_text(GTK_ENTRY(entry), new_text);
+		g_free(new_text);
+	}
 }
 
-int 
-main(int argc, char *argv[]) 
+/* Main function to set up the GTK application and run the main loop. */
+int
+main(int argc, char *argv[])
 {
-    GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *button;
-    const gchar *buttons[] = {
-        "7", "8", "9", "/",
-        "4", "5", "6", "*",
-        "1", "2", "3", "-",
-        "0", ".", "(", ")",
-        "C", "=", "+", ""}; // 括弧とクリアボタンを追加
+	GtkWidget *window;
+	GtkWidget *grid;
+	GtkWidget *button;
+	const gchar *buttons[] = {
+		"7", "8", "9", "/",
+		"4", "5", "6", "*",
+		"1", "2", "3", "-",
+		"0", ".", "(", ")",
+		"C", "=", "+", ""};
 
-    gtk_init(&argc, &argv);
+	gtk_init(&argc, &argv);
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Calculator");
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 250);
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "Calculator");
+	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+	gtk_window_set_default_size(GTK_WINDOW(window), 300, 250);
 
-    grid = gtk_grid_new();
-    gtk_container_add(GTK_CONTAINER(window), grid);
+	grid = gtk_grid_new();
+	gtk_container_add(GTK_CONTAINER(window), grid);
 
-    expression_label = gtk_label_new("");
-    gtk_label_set_justify(GTK_LABEL(expression_label), GTK_JUSTIFY_LEFT);
-    gtk_grid_attach(GTK_GRID(grid), expression_label, 0, 0, 4, 1);
+	expression_label = gtk_label_new("");
+	gtk_label_set_justify(GTK_LABEL(expression_label), GTK_JUSTIFY_LEFT);
+	gtk_grid_attach(GTK_GRID(grid), expression_label, 0, 0, 4, 1);
 
-    entry = gtk_entry_new();
-    gtk_grid_attach(GTK_GRID(grid), entry, 0, 1, 4, 1);
+	entry = gtk_entry_new();
+	gtk_grid_attach(GTK_GRID(grid), entry, 0, 1, 4, 1);
 
-    for (int i = 0; i < 5; ++i) { // 行数を5に増やす
-        for (int j = 0; j < 4; ++j) {
-            if (*buttons[i * 4 + j] == '\0') continue; // 空文字列の場合はボタンを作成しない
-            button = gtk_button_new_with_label(buttons[i * 4 + j]);
-            g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
-            gtk_grid_attach(GTK_GRID(grid), button, j, i + 2, 1, 1);
-        }
-    }
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			if (*buttons[i * 4 + j] == '\0') continue;
+			button = gtk_button_new_with_label(buttons[i * 4 + j]);
+			g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+			gtk_grid_attach(GTK_GRID(grid), button, j, i + 2, 1, 1);
+		}
+	}
 
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_widget_show_all(window);
-    gtk_main();
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	gtk_widget_show_all(window);
+	gtk_main();
 
-    return 0;
+	return 0;
 }
-
