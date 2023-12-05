df1c7eedcccb49d07d859229e3be91a06f53b308
Unixtech-06 <ryosuke_666@icloud.com>
2023-12-05 13:45:37 +0900

    update src

--- src/expression_parser.c
+++ src/expression_parser.c
@@ -39,146 +39,125 @@
 static int
 get_precedence(char op)
 {
-    switch (op)
-    {
-    case '*':
-    case '/':
-        return 2;
-    case '+':
-    case '-':
-        return 1;
-    default:
-        return 0;
-    }
+	switch (op) {
+	case '*':
+	case '/':
+		return 2;
+	case '+':
+	case '-':
+		return 1;
+	default:
+		return 0;
+	}
 }
 
 static double
 apply_operator(double val1, double val2, char op, int *error)
 {
-    *error = 0; // Reset error flag
+	*error = 0; /* Reset error flag */
 
-    switch (op)
-    {
-    case '+':
-        return val1 + val2;
-    case '-':
-        return val1 - val2;
-    case '*':
-        return val1 * val2;
-    case '/':
-        if (val2 == 0)
-        {
-            fprintf(stderr, "Error: Division by zero.\n");
-            *error = 1; // Set error flag
-            return 0.0;
-        }
-        return val1 / val2;
-    default:
-        *error = 1; // Set error flag for unknown operator
-        return 0.0;
-    }
+	switch (op) {
+	case '+':
+		return val1 + val2;
+	case '-':
+		return val1 - val2;
+	case '*':
+		return val1 * val2;
+	case '/':
+		if (val2 == 0) {
+			fprintf(stderr, "Error: Division by zero.\n");
+			*error = 1; /* Set error flag */
+			return 0.0;
+		}
+		return val1 / val2;
+	default:
+		*error = 1; /* Set error flag for unknown operator */
+		return 0.0;
+	}
 }
 
 double
 calculate_with_parentheses(const char *expression)
 {
-    double numbers[100];           // Stack for numbers
-    char operators[100];           // Stack for operators
-    int num_top = -1, op_top = -1; // Stack pointers
-    int error_flag = 0;
-    int open_parentheses = 0;
+	double numbers[100]; /* Stack for numbers */
+	char operators[100]; /* Stack for operators */
+	int num_top = -1, op_top = -1; /* Stack pointers */
+	int error_flag = 0;
+	int open_parentheses = 0;
 
-    for (int i = 0; expression[i] != '\0'; i++)
-    {
-        if (isspace(expression[i]))
-            continue;
+	for (int i = 0; expression[i] != '\0'; i++) {
+		if (isspace(expression[i]))
+			continue;
 
-        if (isdigit(expression[i]))
-        {
-            double value = 0;
-            while (isdigit(expression[i]))
-            {
-                value = value * 10 + (expression[i] - '0');
-                i++;
-            }
-            i--;
-            numbers[++num_top] = value;
-        }
-        else if (expression[i] == '(')
-        {
-            operators[++op_top] = expression[i];
-            open_parentheses++;
-        }
-        else if (expression[i] == ')')
-        {
-            if (open_parentheses <= 0)
-            {
-                fprintf(stderr, "Error: Unmatched closing parenthesis.\n");
-                return 0.0;
-            }
-            while (op_top != -1 && operators[op_top] != '(')
-            {
-                double val2 = numbers[num_top--];
-                double val1 = numbers[num_top--];
-                char op = operators[op_top--];
+		if (isdigit(expression[i])) {
+			double value = 0;
+			while (isdigit(expression[i])) {
+				value = value * 10 + (expression[i] - '0');
+				i++;
+			}
+			i--;
+			numbers[++num_top] = value;
+		} else if (expression[i] == '(') {
+			operators[++op_top] = expression[i];
+			open_parentheses++;
+		} else if (expression[i] == ')') {
+			if (open_parentheses <= 0) {
+				fprintf(stderr, "Error: Unmatched closing parenthesis.\n");
+				return 0.0;
+			}
+			while (op_top != -1 && operators[op_top] != '(') {
+				double val2 = numbers[num_top--];
+				double val1 = numbers[num_top--];
+				char op = operators[op_top--];
 
-                numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
-                if (error_flag)
-                {
-                    fprintf(stderr, "Error: Invalid operation.\n");
-                    return 0.0;
-                }
-            }
-            if (op_top != -1)
-            {
-                op_top--; // Pop the '(' from the stack
-            }
-            open_parentheses--;
-        }
-        else
-        {
-            if (get_precedence(expression[i]) == 0)
-            {
-                fprintf(stderr, "Error: Invalid operator '%c'.\n", expression[i]);
-                return 0.0;
-            }
-            while (op_top != -1 &&
-                   get_precedence(operators[op_top]) >= get_precedence(expression[i]))
-            {
-                double val2 = numbers[num_top--];
-                double val1 = numbers[num_top--];
-                char op = operators[op_top--];
+				numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
+				if (error_flag) {
+					fprintf(stderr, "Error: Invalid operation.\n");
+					return 0.0;
+				}
+			}
+			if (op_top != -1) {
+				op_top--; /* Pop the '(' from the stack */
+			}
+			open_parentheses--;
+		} else {
+			if (get_precedence(expression[i]) == 0) {
+				fprintf(stderr, "Error: Invalid operator '%c'.\n", expression[i]);
+				return 0.0;
+			}
+			while (op_top != -1 &&
+			       get_precedence(operators[op_top]) >= get_precedence(expression[i])) {
+				double val2 = numbers[num_top--];
+				double val1 = numbers[num_top--];
+				char op = operators[op_top--];
 
-                numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
-                if (error_flag)
-                {
-                    fprintf(stderr, "Error: Invalid operation.\n");
-                    return 0.0;
-                }
-            }
-            operators[++op_top] = expression[i];
-        }
-    }
+				numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
+				if (error_flag) {
+					fprintf(stderr, "Error: Invalid operation.\n");
+					return 0.0;
+				}
+			}
+			operators[++op_top] = expression[i];
+		}
+	}
 
-    if (open_parentheses != 0)
-    {
-        fprintf(stderr, "Error: Unmatched opening parenthesis.\n");
-        return 0.0;
-    }
+	if (open_parentheses != 0) {
+		fprintf(stderr, "Error: Unmatched opening parenthesis.\n");
+		return 0.0;
+	}
 
-    while (op_top != -1)
-    {
-        double val2 = numbers[num_top--];
-        double val1 = numbers[num_top--];
-        char op = operators[op_top--];
+	while (op_top != -1) {
+		double val2 = numbers[num_top--];
+		double val1 = numbers[num_top--];
+		char op = operators[op_top--];
 
-        numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
-        if (error_flag)
-        {
-            fprintf(stderr, "Error: Invalid operation.\n");
-            return 0.0;
-        }
-    }
+		numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
+		if (error_flag) {
+			fprintf(stderr, "Error: Invalid operation.\n");
+			return 0.0;
+		}
+	}
 
-    return numbers[num_top];
+	return numbers[num_top];
 }
+
--- src/main.c
+++ src/main.c
@@ -45,251 +45,254 @@ static GtkWidget *expression_label;
 static int 
 get_precedence(char op) 
 {
-    switch (op) {
-        case '*':
-        case '/':
-            return 2;
-        case '+':
-        case '-':
-            return 1;
-        default:
-            return 0;
-    }
+	switch (op) {
+	case '*':
+	case '/':
+		return 2;
+	case '+':
+	case '-':
+		return 1;
+	default:
+		return 0;
+	}
 }
 
 static double 
 apply_operator(double val1, double val2, char op, int *error) 
 {
-    *error = 0; // Reset error flag
+	*error = 0; /* Reset error flag */
 
-    switch (op) {
-        case '+': return val1 + val2;
-        case '-': return val1 - val2;
-        case '*': return val1 * val2;
-        case '/': 
-            if (val2 == 0) {
-                fprintf(stderr, "Error: Division by zero.\n");
-                *error = 1; // Set error flag
-                return 0.0;
-            }
-            return val1 / val2;
-        default: 
-            *error = 1; // Set error flag for unknown operator
-            return 0.0;
-    }
+	switch (op) {
+	case '+': return val1 + val2;
+	case '-': return val1 - val2;
+	case '*': return val1 * val2;
+	case '/': 
+		if (val2 == 0) {
+			fprintf(stderr, "Error: Division by zero.\n");
+			*error = 1; /* Set error flag */
+			return 0.0;
+		}
+		return val1 / val2;
+	default: 
+		*error = 1; /* Set error flag for unknown operator */
+		return 0.0;
+	}
 }
 
 static void 
 resize_stack(double **stack, int *size) 
 {
-    *size *= 2;
-    *stack = realloc(*stack, *size * sizeof(double));
-    if (*stack == NULL) {
-        fprintf(stderr, "Error: Stack memory allocation failed.\n");
-        exit(EXIT_FAILURE);
-    }
+	*size *= 2;
+	*stack = realloc(*stack, *size * sizeof(double));
+	if (*stack == NULL) {
+		fprintf(stderr, "Error: Stack memory allocation failed.\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
 static double 
 calculate_with_parentheses(const char *expression) 
 {
-    int stack_size = INITIAL_STACK_SIZE;
-    double *numbers = malloc(stack_size * sizeof(double)); // Dynamic stack for numbers
-    char *operators = malloc(stack_size * sizeof(char)); // Dynamic stack for operators
-    if (numbers == NULL || operators == NULL) {
-        fprintf(stderr, "Error: Initial stack memory allocation failed.\n");
-        exit(EXIT_FAILURE);
-    }
+	int stack_size = INITIAL_STACK_SIZE;
+	double *numbers = malloc(stack_size * sizeof(double)); /* Dynamic stack for numbers */
+	char *operators = malloc(stack_size * sizeof(char)); /* Dynamic stack for operators */
+	if (numbers == NULL || operators == NULL) {
+		fprintf(stderr, "Error: Initial stack memory allocation failed.\n");
+		exit(EXIT_FAILURE);
+	}
 
-    int num_top = -1, op_top = -1; // Stack pointers
-    int error_flag = 0;
-    int open_parentheses = 0;
+	int num_top = -1, op_top = -1; /* Stack pointers */
+	int error_flag = 0;
+	int open_parentheses = 0;
 
-    for (int i = 0; expression[i] != '\0'; i++) {
-        if (isspace(expression[i]))
-            continue;
+	for (int i = 0; expression[i] != '\0'; i++) {
+		if (isspace(expression[i]))
+			continue;
 
-        if (isdigit(expression[i])) {
-            double value = 0;
-            while (isdigit(expression[i])) {
-                value = value * 10 + (expression[i] - '0');
-                i++;
-            }
-            i--;
-            if (num_top == stack_size - 1) {
-                resize_stack(&numbers, &stack_size);
-            }
-            numbers[++num_top] = value;
-        } else if (expression[i] == '(') {
-            if (op_top == stack_size - 1) {
-                resize_stack((double **)&operators, &stack_size);
-            }
-            operators[++op_top] = expression[i];
-            open_parentheses++;
-        } else if (expression[i] == ')') {
-            while (op_top != -1 && operators[op_top] != '(') {
-                double val2 = numbers[num_top--];
-                double val1 = numbers[num_top--];
-                char op = operators[op_top--];
+		if (isdigit(expression[i])) {
+			double value = 0;
+			while (isdigit(expression[i])) {
+				value = value * 10 + (expression[i] - '0');
+				i++;
+			}
+			i--;
+			if (num_top == stack_size - 1) {
+				resize_stack(&numbers, &stack_size);
+			}
+			numbers[++num_top] = value;
+		} else if (expression[i] == '(') {
+			if (op_top == stack_size - 1) {
+				resize_stack((double **)&operators, &stack_size);
+			}
+			operators[++op_top] = expression[i];
+			open_parentheses++;
+		} else if (expression[i] == ')') {
+			while (op_top != -1 && operators[op_top] != '(') {
+				double val2 = numbers[num_top--];
+				double val1 = numbers[num_top--];
+				char op = operators[op_top--];
 
-                if (num_top == stack_size - 1) {
-                    resize_stack(&numbers, &stack_size);
-                }
-                numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
-                if (error_flag) {
-                    free(numbers);
-                    free(operators);
-                    fprintf(stderr, "Error: Invalid operation.\n");
-                    return 0.0;
-                }
-            }
-            if (op_top != -1) {
-                op_top--; // Pop the '(' from the stack
-            }
-            open_parentheses--;
-        } else {
-            while (op_top != -1 && 
-                   get_precedence(operators[op_top]) >= get_precedence(expression[i])) {
-                double val2 = numbers[num_top--];
-                double val1 = numbers[num_top--];
-                char op = operators[op_top--];
+				if (num_top == stack_size - 1) {
+					resize_stack(&numbers, &stack_size);
+				}
+				numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
+				if (error_flag) {
+					free(numbers);
+					free(operators);
+					fprintf(stderr, "Error: Invalid operation.\n");
+					return 0.0;
+				}
+			}
+			if (op_top != -1) {
+				op_top--; /* Pop the '(' from the stack */
+			}
+			open_parentheses--;
+		} else {
+			while (op_top != -1 && 
+				   get_precedence(operators[op_top]) >= get_precedence(expression[i])) {
+				double val2 = numbers[num_top--];
+				double val1 = numbers[num_top--];
+				char op = operators[op_top--];
 
-                if (num_top == stack_size - 1) {
-                    resize_stack(&numbers, &stack_size);
-                }
-                numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
-                if (error_flag) {
-                    free(numbers);
-                    free(operators);
-                    fprintf(stderr, "Error: Invalid operation.\n");
-                    return 0.0;
-                }
-            }
-            if (op_top == stack_size - 1) {
-                resize_stack((double **)&operators, &stack_size);
-            }
-            operators[++op_top] = expression[i];
-        }
-    }
+				if (num_top == stack_size - 1) {
+					resize_stack(&numbers, &stack_size);
+				}
+				numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
+				if (error_flag) {
+					free(numbers);
+					free(operators);
+					fprintf(stderr, "Error: Invalid operation.\n");
+					return 0.0;
+				}
+			}
+			if (op_top == stack_size - 1) {
+				resize_stack((double **)&operators, &stack_size);
+			}
+			operators[++op_top] = expression[i];
+		}
+	}
 
-    if (open_parentheses != 0) {
-        free(numbers);
-        free(operators);
-        fprintf(stderr, "Error: Unmatched parentheses.\n");
-        return 0.0;
-    }
+	if (open_parentheses != 0) {
+		free(numbers);
+		free(operators);
+		fprintf(stderr, "Error: Unmatched parentheses.\n");
+		return 0.0;
+	}
 
-    while (op_top != -1) {
-        double val2 = numbers[num_top--];
-        double val1 = numbers[num_top--];
-        char op = operators[op_top--];
+	while (op_top != -1) {
+		double val2 = numbers[num_top--];
+		double val1 = numbers[num_top--];
+		char op = operators[op_top--];
 
-        if (num_top == stack_size - 1) {
-            resize_stack(&numbers, &stack_size);
-        }
-        numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
-        if (error_flag) {
-            free(numbers);
-            free(operators);
-            fprintf(stderr, "Error: Invalid operation.\n");
-            return 0.0;
-        }
-    }
+		if (num_top == stack_size - 1) {
+			resize_stack(&numbers, &stack_size);
+		}
+		numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
+		if (error_flag) {
+			free(numbers);
+			free(operators);
+			fprintf(stderr, "Error: Invalid operation.\n");
+			return 0.0;
+		}
+	}
 
-    double result = numbers[num_top];
-    free(numbers);
-    free(operators);
-    return result;
+	double result = numbers[num_top];
+	free(numbers);
+	free(operators);
+	return result;
 }
 
 
 static void 
 on_button_clicked(GtkWidget *widget, gpointer data)
 {
-    const gchar *label = gtk_button_get_label(GTK_BUTTON(widget));
-    if (label == NULL) {
-        fprintf(stderr, "Error: Null label in button.\n");
-        return;
-    }
+	const gchar *label = gtk_button_get_label(GTK_BUTTON(widget));
+	if (label == NULL) {
+		fprintf(stderr, "Error: Null label in button.\n");
+		return;
+	}
 
-    const gchar *current_text = gtk_entry_get_text(GTK_ENTRY(entry));
-    if (current_text == NULL) {
-        fprintf(stderr, "Error: Null text in entry widget.\n");
-        return;
-    }
+	const gchar *current_text = gtk_entry_get_text(GTK_ENTRY(entry));
+	if (current_text == NULL) {
+		fprintf(stderr, "Error: Null text in entry widget.\n");
+		return;
+	}
 
-    // Validate the input expression
-    for (int i = 0; current_text[i] != '\0'; i++) {
-        if (!isdigit(current_text[i]) && !isspace(current_text[i]) &&
-            strchr("+-*/()", current_text[i]) == NULL) {
-            fprintf(stderr, "Error: Invalid input character '%c'.\n", current_text[i]);
-            return;
-        }
-    }
+	/* Validate the input expression */
+	for (int i = 0; current_text[i] != '\0'; i++) {
+		if (!isdigit(current_text[i]) && !isspace(current_text[i]) &&
+		    strchr("+-*/()", current_text[i]) == NULL) {
+			fprintf(stderr, "Error: Invalid input character '%c'.\n", current_text[i]);
+			return;
+		}
+	}
 
-    if (strcmp(label, "=") == 0) {
-        gtk_label_set_text(GTK_LABEL(expression_label), current_text);
+	if (strcmp(label, "=") == 0) {
+		gtk_label_set_text(GTK_LABEL(expression_label), current_text);
 
-        double result = calculate_with_parentheses(current_text);
-        char result_str[64];
-        snprintf(result_str, sizeof(result_str), "%f", result);
-        gtk_entry_set_text(GTK_ENTRY(entry), result_str);
-    } else {
-        gchar *new_text = g_strdup_printf("%s%s", current_text, label);
-        if (new_text == NULL) {
-            fprintf(stderr, "Error: Failed to allocate memory for new text.\n");
-            return;
-        }
-        gtk_entry_set_text(GTK_ENTRY(entry), new_text);
-        g_free(new_text);
-    }
+		double result = calculate_with_parentheses(current_text);
+		char result_str[64];
+		snprintf(result_str, sizeof(result_str), "%f", result);
+		gtk_entry_set_text(GTK_ENTRY(entry), result_str);
+	} else {
+		gchar *new_text = g_strdup_printf("%s%s", current_text, label);
+		if (new_text == NULL) {
+			fprintf(stderr, "Error: Failed to allocate memory for new text.\n");
+			return;
+		}
+		gtk_entry_set_text(GTK_ENTRY(entry), new_text);
+		g_free(new_text);
+	}
 }
+
 
 int 
 main(int argc, char *argv[])
 {
-    GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *button;
-    const gchar *buttons[] = {
-        "7", "8", "9", "/",
-        "4", "5", "6", "*",
-        "1", "2", "3", "-",
-        "0", ".", "(", ")",
-        "C", "=", "+", ""
-    };
+	GtkWidget *window;
+	GtkWidget *grid;
+	GtkWidget *button;
+	const gchar *buttons[] = {
+		"7", "8", "9", "/",
+		"4", "5", "6", "*",
+		"1", "2", "3", "-",
+		"0", ".", "(", ")",
+		"C", "=", "+", ""
+	};
 
-    gtk_init(&argc, &argv);
+	gtk_init(&argc, &argv);
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Calculator");
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 250);
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "Calculator");
+	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+	gtk_window_set_default_size(GTK_WINDOW(window), 300, 250);
 
-    grid = gtk_grid_new();
-    gtk_container_add(GTK_CONTAINER(window), grid);
+	grid = gtk_grid_new();
+	gtk_container_add(GTK_CONTAINER(window), grid);
 
-    expression_label = gtk_label_new("");
-    gtk_label_set_justify(GTK_LABEL(expression_label), GTK_JUSTIFY_LEFT);
-    gtk_grid_attach(GTK_GRID(grid), expression_label, 0, 0, 4, 1);
+	expression_label = gtk_label_new("");
+	gtk_label_set_justify(GTK_LABEL(expression_label), GTK_JUSTIFY_LEFT);
+	gtk_grid_attach(GTK_GRID(grid), expression_label, 0, 0, 4, 1);
 
-    entry = gtk_entry_new();
-    gtk_grid_attach(GTK_GRID(grid), entry, 0, 1, 4, 1);
+	entry = gtk_entry_new();
+	gtk_grid_attach(GTK_GRID(grid), entry, 0, 1, 4, 1);
 
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            if (*buttons[i * 4 + j] == '\0') continue;
-            button = gtk_button_new_with_label(buttons[i * 4 + j]);
-            g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
-            gtk_grid_attach(GTK_GRID(grid), button, j, i + 2, 1, 1);
-        }
-    }
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			if (*buttons[i * 4 + j] == '\0') 
+				continue;
+			button = gtk_button_new_with_label(buttons[i * 4 + j]);
+			g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+			gtk_grid_attach(GTK_GRID(grid), button, j, i + 2, 1, 1);
+		}
+	}
 
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_widget_show_all(window);
-    gtk_main();
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	gtk_widget_show_all(window);
+	gtk_main();
 
-    return 0;
+	return 0;
 }
+
 
 
