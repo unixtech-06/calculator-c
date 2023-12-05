da5f9200f5ce30f7191af7f4a73bd102edda4ee7
Unixtech-06 <ryosuke_666@icloud.com>
2023-12-05 13:12:56 +0900

    more secure main.c

--- src/main.c
+++ src/main.c
@@ -32,10 +32,176 @@
 
 
 #include <gtk/gtk.h>
-#include "../lib/expression_parser.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#define INITIAL_STACK_SIZE 100
 
 static GtkWidget *entry;
 static GtkWidget *expression_label;
+
+static int 
+get_precedence(char op) 
+{
+    switch (op) {
+        case '*':
+        case '/':
+            return 2;
+        case '+':
+        case '-':
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+static double 
+apply_operator(double val1, double val2, char op, int *error) 
+{
+    *error = 0; // Reset error flag
+
+    switch (op) {
+        case '+': return val1 + val2;
+        case '-': return val1 - val2;
+        case '*': return val1 * val2;
+        case '/': 
+            if (val2 == 0) {
+                fprintf(stderr, "Error: Division by zero.\n");
+                *error = 1; // Set error flag
+                return 0.0;
+            }
+            return val1 / val2;
+        default: 
+            *error = 1; // Set error flag for unknown operator
+            return 0.0;
+    }
+}
+
+static void 
+resize_stack(double **stack, int *size) 
+{
+    *size *= 2;
+    *stack = realloc(*stack, *size * sizeof(double));
+    if (*stack == NULL) {
+        fprintf(stderr, "Error: Stack memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+static double 
+calculate_with_parentheses(const char *expression) 
+{
+    int stack_size = INITIAL_STACK_SIZE;
+    double *numbers = malloc(stack_size * sizeof(double)); // Dynamic stack for numbers
+    char *operators = malloc(stack_size * sizeof(char)); // Dynamic stack for operators
+    if (numbers == NULL || operators == NULL) {
+        fprintf(stderr, "Error: Initial stack memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int num_top = -1, op_top = -1; // Stack pointers
+    int error_flag = 0;
+    int open_parentheses = 0;
+
+    for (int i = 0; expression[i] != '\0'; i++) {
+        if (isspace(expression[i]))
+            continue;
+
+        if (isdigit(expression[i])) {
+            double value = 0;
+            while (isdigit(expression[i])) {
+                value = value * 10 + (expression[i] - '0');
+                i++;
+            }
+            i--;
+            if (num_top == stack_size - 1) {
+                resize_stack(&numbers, &stack_size);
+            }
+            numbers[++num_top] = value;
+        } else if (expression[i] == '(') {
+            if (op_top == stack_size - 1) {
+                resize_stack((double **)&operators, &stack_size);
+            }
+            operators[++op_top] = expression[i];
+            open_parentheses++;
+        } else if (expression[i] == ')') {
+            while (op_top != -1 && operators[op_top] != '(') {
+                double val2 = numbers[num_top--];
+                double val1 = numbers[num_top--];
+                char op = operators[op_top--];
+
+                if (num_top == stack_size - 1) {
+                    resize_stack(&numbers, &stack_size);
+                }
+                numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
+                if (error_flag) {
+                    free(numbers);
+                    free(operators);
+                    fprintf(stderr, "Error: Invalid operation.\n");
+                    return 0.0;
+                }
+            }
+            if (op_top != -1) {
+                op_top--; // Pop the '(' from the stack
+            }
+            open_parentheses--;
+        } else {
+            while (op_top != -1 && 
+                   get_precedence(operators[op_top]) >= get_precedence(expression[i])) {
+                double val2 = numbers[num_top--];
+                double val1 = numbers[num_top--];
+                char op = operators[op_top--];
+
+                if (num_top == stack_size - 1) {
+                    resize_stack(&numbers, &stack_size);
+                }
+                numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
+                if (error_flag) {
+                    free(numbers);
+                    free(operators);
+                    fprintf(stderr, "Error: Invalid operation.\n");
+                    return 0.0;
+                }
+            }
+            if (op_top == stack_size - 1) {
+                resize_stack((double **)&operators, &stack_size);
+            }
+            operators[++op_top] = expression[i];
+        }
+    }
+
+    if (open_parentheses != 0) {
+        free(numbers);
+        free(operators);
+        fprintf(stderr, "Error: Unmatched parentheses.\n");
+        return 0.0;
+    }
+
+    while (op_top != -1) {
+        double val2 = numbers[num_top--];
+        double val1 = numbers[num_top--];
+        char op = operators[op_top--];
+
+        if (num_top == stack_size - 1) {
+            resize_stack(&numbers, &stack_size);
+        }
+        numbers[++num_top] = apply_operator(val1, val2, op, &error_flag);
+        if (error_flag) {
+            free(numbers);
+            free(operators);
+            fprintf(stderr, "Error: Invalid operation.\n");
+            return 0.0;
+        }
+    }
+
+    double result = numbers[num_top];
+    free(numbers);
+    free(operators);
+    return result;
+}
+
 
 static void 
 on_button_clicked(GtkWidget *widget, gpointer data)
@@ -116,4 +282,5 @@ main(int argc, char *argv[])
 
     return 0;
 }
+
 
